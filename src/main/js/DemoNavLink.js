af6e41e54210758d60b2572ed10eea5125f9be97
Florian Scholdei <florian.scholdei@cloudogu.com>
2019-10-02 11:31:37 +0200

    correct dumb copy&paste errors, remove unused imports

--- src/main/js/DemoNavLink.js
+++ src/main/js/DemoNavLink.js
@@ -1,18 +1,9 @@
 // @flow
 import React from "react";
-import { translate } from "react-i18next";
 import { NavLink } from "@scm-manager/ui-components";
 
 export default class DemoNavLink extends React.Component<> {
   render() {
-    const { t } = this.props;
-
-    return (
-      <NavLink
-        to="/admin/demo"
-        icon="fas fa-demo"
-        label="Demo"
-      />
-    );
+    return <NavLink to="/admin/demo" icon="fas fa-paint-brush" label="Demo" />;
   }
 }
--- src/main/js/index.js
+++ src/main/js/index.js
@@ -2,25 +2,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { binder } from "@scm-manager/ui-extensions";
-import type { Links } from "@scm-manager/ui-types";
 import DemoNavLink from "./DemoNavLink";
 import DemoPage from "./DemoPage";
 
-type PredicateProps = {
-  links: Links
+const DemoRoute = () => {
+  return <Route path="/admin/demo" render={() => <DemoPage />} />;
 };
 
-const Route = ({ links }) => {
-  return (
-    <Route
-      path="/admin/demo"
-      render={() => (
-        <DemoPage />
-      )}
-    />
-  );
-};
-
-binder.bind("admin.route", Route);
+binder.bind("admin.route", DemoRoute);
 
 binder.bind("admin.navigation", DemoNavLink);
